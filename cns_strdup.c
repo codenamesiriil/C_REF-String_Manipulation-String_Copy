49fa26b13c7cbcd2fcabc81dff44d73226a2ad7c
codenamesiriil <codenamesiriil@gmail.com>
2024-09-06 08:30:30 +0100

    cns_strdup

---
+++ cns_strdup.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+static size_t	cns_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*cns_strdup(const char *s)
+{
+	char	*new_str;
+	int		i;
+
+	new_str = (char *)malloc(cns_strlen(s) + 1);
+	if (!new_str)
+		return (NULL);
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		new_str[i] = s[i];
+		i++;
+	}
+	new_str[i] = '\0';
+
+	return (new_str);
+}
